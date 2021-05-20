40c132abab499eea973b543fc9c80df6305c1083
Barbara Diaz <babs@pop-os.localdomain>
2021-05-19 22:23:26 -0500

    create main.py file

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__) 
+
+app.config["DEBUG"] = True
+
+#Upload folder
+UPLOAD_FOLDER = 'static/files'
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+def parseCSV(filepath):
+    csvData = pd.read_csv(filepath)
+    print(csvData.head())
+    return "file loaded"
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+#get the uoloaded file
+@app.route("/", methods = ["POST"])
+def upload_file():
+    uploaded_file = request.files["file"]
+    if uploaded_file.filename != "":
+        file_path = os.path.join(app.config["UPLOAD_FOLDER"], uploaded_file.filename)
+        uploaded_file.save(file_path)
+        #read_csv
+        parseCSV(file_path)
+    return redirect(url_for("index"))
+
+
+if (__name__ == "__main__"):
+    app.run(port=5000) 
